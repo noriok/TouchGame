ad91e6c9b9c47b795a19393a25f279e339ff28c2
Norio Kobayashi <noriokob@gmail.com>
2016-01-17 22:55:00 +0900

    Update

---
+++ Assets/Scripts/Circle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Circle : MonoBehaviour, IPointerClickHandler {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void OnPointerClick(PointerEventData eventData) {
+        if (eventData.clickCount == 1) {
+            // クリックされたら自身を消す
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/MainSystem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class MainSystem : MonoBehaviour {
+    public GameObject _circle;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+        // 画面上に表示されている円の個数
+        if (0 == GameObject.FindGameObjectsWithTag("Circle").Count()) {
+            var x = Random.Range(-8, 8);
+            var y = Random.Range(-4, 4);
+            Instantiate(_circle, new Vector3(x, y, 0), Quaternion.identity);
+        }
+	}
+}
